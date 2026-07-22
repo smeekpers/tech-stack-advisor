51d2853a895d91e8b93d989670ee78bf997a2210
smeekpers <stephen@stephenjmeek.onmicrosoft.com>
2026-07-22 23:38:04 +0100

    changes

--- app.py
+++ app.py
@@ -159,4 +159,7 @@ demo = gr.Interface(
 # -------------------------------------
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=7860
+    )
